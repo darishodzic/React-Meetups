5946f229b015dbe172a8b6f2ad98748cea77f15b
Daris <daris.hodzic.dh@gmail.com>
2021-09-15 15:05:45 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+	Route,
+	Switch,
+} from "react-router-dom";
+import AllMeetupsPage from "./page/AllMeetups";
+import NewMeetupsPage from "./page/NewMeetups";
+import FavoritesPage from "./page/Favorites";
+import MainNavigation from "./components/layout/MainNavigation";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div>
+			<MainNavigation />
+			<Switch>
+				<Route path="/" exact>
+					<AllMeetupsPage />
+				</Route>
+				<Route path="/new-meetup">
+					<NewMeetupsPage />
+				</Route>
+				<Route path="/favorites">
+					<FavoritesPage />
+				</Route>
+			</Switch>
+		</div>
+	);
 }
 
 export default App;
---
+++ src/page/NewMeetups.js
@@ -0,0 +1,5 @@
+function NewMeetupsPage() {
+	return <div>New Meetups Diiv</div>;
+}
+
+export default NewMeetupsPage;
