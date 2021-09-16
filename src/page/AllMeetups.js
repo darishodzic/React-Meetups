76fa83c0f4031d656a40bcebd597d86fdea23d3c
Daris <daris.hodzic.dh@gmail.com>
2021-09-16 18:48:56 +0200

    fetching from firebase

--- src/page/AllMeetups.js
+++ src/page/AllMeetups.js
@@ -1,3 +1,7 @@
+import {
+	useState,
+	useEffect,
+} from "react";
 import MeetupList from "../components/meetups/MeetupList";
 
 const DUMMY_DATA = [
@@ -24,11 +28,49 @@ const DUMMY_DATA = [
 ];
 
 function AllMeetupsPage() {
+	const [isLoading, setIsLoading] =
+		useState(true);
+
+	const [
+		loadedMeetups,
+		setLoadedMeetups,
+	] = useState([]);
+
+	useEffect(() => {
+		setIsLoading(true);
+		fetch(
+			"https://react-meetups-61b0f-default-rtdb.firebaseio.com/meetups.json"
+		)
+			.then((response) => {
+				return response.json();
+			})
+			.then((data) => {
+				const meetups = [];
+				for (const key in data) {
+					const meetup = {
+						id: key,
+						...data[key],
+					};
+					meetups.push(meetup);
+				}
+				setIsLoading(false);
+				setLoadedMeetups(meetups);
+			});
+	}, []);
+
+	if (isLoading) {
+		return (
+			<section>
+				<p>Loading...</p>
+			</section>
+		);
+	}
+
 	return (
 		<section>
 			<h1>All Meetups</h1>
 			<MeetupList
-				meetups={DUMMY_DATA}
+				meetups={loadedMeetups}
 			/>
 		</section>
 	);
